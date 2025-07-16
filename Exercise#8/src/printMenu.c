9d28532b90f99f97631f560edd5f859877aa04de
TackaIzHe <vitalia_mikhailov@mail.ru>
2025-07-16 06:42:55 +0000

    develop

---
+++ Exercise#8/src/printMenu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int printMenu(){
+    printf("1 Сложение \n");
+    printf("2 Вычитание \n");
+    printf("3 Умножение \n");
+    printf("4 Деление \n");
+    printf("5 Выход \n");
+    return 0;
+}
