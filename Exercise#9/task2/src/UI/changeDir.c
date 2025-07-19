de278680f0578bfd68f6e504166510ccb5fb4ea2
TackaIzHe <vitalia_mikhailov@mail.ru>
2025-07-19 10:37:35 +0000

    develop

--- Exercise#9/task2/src/UI/changeDir.c
+++ Exercise#9/task2/src/UI/changeDir.c
@@ -3,9 +3,11 @@
 #include "../getFile.h"
 #include "../main.h"
 
-int changeDir(char** path, struct FileList** files, int selectLine){
-        chdir(getItem(files,selectLine+1)->name);
-        getPath(path);
-        getFileAndDir(files,*path);
+int changeDir(char **path, struct FileList **files, int selectLine)
+{
+    chdir(*path);
+    chdir(getItem(files, selectLine + 1)->name);
+    getPath(path);
+    getFileAndDir(files, *path);
     return 0;
 }
