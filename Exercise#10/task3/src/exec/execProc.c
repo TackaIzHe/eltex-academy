316d0fd509cf1add55612ae090cf801ea0c3d427
TackaIzHe <vitalia_mikhailov@mail.ru>
2025-07-30 06:51:27 +0000

    develop

--- Exercise#10/task3/src/exec/execProc.c
+++ Exercise#10/task3/src/exec/execProc.c
@@ -14,21 +14,12 @@ int execProc(char** atr,int lenght){
     strcat(binPath,"/bin/");
 
     strcat(binPath,atr[0]);
+    atr = realloc(atr,(lenght+1) * 8);
+    atr[lenght] = malloc(4);
+    atr[lenght] = NULL;
     proc = fork();
     if(proc == 0){
-        execl(binPath,
-            lenght>=0?atr[0]:NULL,
-            lenght>=1?atr[1]:NULL,
-            lenght>=2?atr[2]:NULL,
-            lenght>=3?atr[3]:NULL,
-            lenght>=4?atr[4]:NULL,
-            lenght>=5?atr[5]:NULL,
-            lenght>=6?atr[6]:NULL,
-            lenght>=7?atr[7]:NULL,
-            lenght>=8?atr[8]:NULL,
-            lenght>=9?atr[9]:NULL,
-            lenght>=10?atr[10]:NULL,
-            NULL);
+        execv(binPath, atr);
         free(binPath);
         exit(EXIT_SUCCESS);
         
