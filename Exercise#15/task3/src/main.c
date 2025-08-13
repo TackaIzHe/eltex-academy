4d43edb2e4d6ddb39814a2b281eb2658d94b926d
TackaIzHe <vitalia_mikhailov@mail.ru>
2025-08-13 18:43:10 +0000

    develop

--- Exercise#15/task3/src/main.c
+++ Exercise#15/task3/src/main.c
@@ -1,12 +1,23 @@
 #include <signal.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
 
-void wait_sig(int){
-    
+void i(int){
+
 }
 
 int main(){
+    sigset_t s;
+    int sig = 0;
 
+    signal(SIGUSR1,i);
 
+    sigemptyset(&s);
+    sigaddset(&s,SIGUSR1);
+    while(1){
+        sigwait(&s,&sig);
+        printf("Получен сигнал SIGUSR1\n");
+    }
     exit(EXIT_SUCCESS);
 }
