a83274f791062100d75af428fe69202f74a9e52b
TackaIzHe <vitalia_mikhailov@mail.ru>
2025-09-02 17:11:24 +0000

    develop

--- Exercise#16/fourth/task2/src/client.c
+++ Exercise#16/fourth/task2/src/client.c
@@ -15,19 +15,33 @@
 
 int main(){
     int fd;
+    int one = 1;
     char buff[256];
     struct sockaddr_in server, client;
     socklen_t server_len, client_len;
     struct in_addr server_addr;
-    struct udphdr hdr;
-    char *data = buff + 8;
+    struct udphdr hdr_udp;
+    struct iphdr hdr_ip;
+    char *data = buff + 8 + 20;
     struct iphdr *recfpac_ip;
     struct udphdr *recfpac_udp;
     
-    hdr.source = htons(CLIENT_PORT);
-    hdr.dest = htons(SERVER_PORT);
-    hdr.len = htons(sizeof(hdr) + 4);
-    hdr.check = 0;
+    hdr_ip.version = 4;
+    hdr_ip.ihl = 5;
+    hdr_ip.tos = 0;
+    hdr_ip.tot_len = 32;
+    hdr_ip.id = 0;
+    hdr_ip.frag_off = 0;
+    hdr_ip.ttl = 255;
+    hdr_ip.protocol = 17;
+    hdr_ip.check = 0;
+    hdr_ip.saddr = htonl(INADDR_LOOPBACK);
+    hdr_ip.daddr = htonl(INADDR_LOOPBACK);
+
+    hdr_udp.source = htons(CLIENT_PORT);
+    hdr_udp.dest = htons(SERVER_PORT);
+    hdr_udp.len = htons(sizeof(hdr_udp) + 4);
+    hdr_udp.check = 0;
 
     server_addr.s_addr = htonl(INADDR_LOOPBACK);
 
@@ -44,11 +58,17 @@ int main(){
         HANDLE_ERR("socket err");
     }
 
+    if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) == -1) {
+        close(fd);
+        HANDLE_ERR("setsockopt err");
+    }
+
     memset(buff, 0, sizeof(buff));
-    memcpy(buff, &hdr, sizeof(hdr));
+    memcpy(buff, &hdr_ip, sizeof(hdr_ip));
+    memcpy(buff + sizeof(hdr_ip), &hdr_udp, sizeof(hdr_udp));
     memcpy(data , "HI!", (size_t)4);
 
-    if( sendto(fd, buff, sizeof(hdr) + strlen(data)+1, 0, (struct sockaddr*)&server, sizeof(server)) == -1 ){
+    if( sendto(fd, buff, sizeof(hdr_ip) + sizeof(hdr_udp) + strlen(data)+1, 0, (struct sockaddr*)&server, sizeof(server)) == -1 ){
         close(fd);
         HANDLE_ERR("sendto err");
     }
@@ -63,7 +83,7 @@ int main(){
         recfpac_ip = (struct iphdr*)buff;
         int iphdr_len = recfpac_ip->ihl*4;
         recfpac_udp = (struct udphdr*) buff + iphdr_len;
-        short* sours_port = (short*)(buff + iphdr_len );
+        short* sours_port = (short*)(buff + iphdr_len);
         short* dest_port = (short*)(buff + iphdr_len + 2);
         char* payload = buff + iphdr_len + 8;
         printf("source port: %d\n", htons(*sours_port));
