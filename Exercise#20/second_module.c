be6c922288c62e4a2cac86149ce0cc3befbc231c
TackaIzHe <vitalia_mikhailov@mail.ru>
2025-10-08 11:09:17 +0000

    develop

--- Exercise#20/second_module.c
+++ Exercise#20/second_module.c
@@ -2,18 +2,30 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/fs.h>
-#include <linux/rwlock.h>
+// #include <linux/rwlock.h>
+#include <linux/rwsem.h>
 #include <linux/uacce.h>
 
+// использовал семафоры так как у меня вылетала ошибка адреса при чтении 
+
+
+
 static int major = 0;
-static rwlock_t lock;
-static char test_string[15] = "Hello!\0";
+// static rwlock_t lock;
+static struct rw_semaphore lock;
+static char test_string[15] = "Hello!\n\0";
 
 static ssize_t read(struct file *fd, char __user *buff, size_t size, loff_t *off){
     size_t rc = 0;
-    read_lock(&lock);
+    down_read(&lock);
+    // read_lock(&lock);
+    printk(KERN_INFO "read: buff=%p, size=%zu, off=%lld, test_string='%s' (len=15)\n", 
+           buff, size, *off, test_string);
     rc = simple_read_from_buffer(buff, size, off, test_string, 15);
-    read_unlock(&lock);
+
+    printk(KERN_INFO "read: rc=%zd\n", rc);
+    up_read(&lock);
+    // read_unlock(&lock);
     return rc;
 }
 
@@ -22,9 +34,11 @@ static ssize_t write(struct file *fd, const char __user *buff, size_t size, loff
     if(size > 15){
         return -EINVAL;
     }
-    write_lock(&lock);
+    down_write(&lock);
+    // write_lock(&lock);
     rc = simple_write_to_buffer(test_string, 15, off, buff, size);
-    write_unlock(&lock);
+    // write_unlock(&lock);
+    up_write(&lock);
     return rc;
 }
 
@@ -35,9 +49,10 @@ static struct file_operations fops = {
 };
 
 static int __init start_module(void){
+    // rwlock_init(&lock);
+    init_rwsem(&lock);
     pr_info("Module loaded !!!\n");
-    rwlock_init(&lock);
-    major = register_chrdev(major, "second", &fops);
+    major = register_chrdev(major, "second_module", &fops);
 
     if(major < 0){
         return major;
@@ -48,7 +63,7 @@ static int __init start_module(void){
 }
 
 static void __exit end_module(void){
-    unregister_chrdev(major, "second");
+    unregister_chrdev(major, "second_module");
     pr_info("Module unloaded !!!\n");
 }
 
